f44fb48d76267d38cd6b022ab260a5a464fe584a
Galen Han <Mastergalen@users.noreply.github.com>
2016-04-03 01:19:08 +0200

    Update complex for loop with other arithmetic operators

--- src/comp207p/target/ComplexForLoop.java
+++ src/comp207p/target/ComplexForLoop.java
@@ -8,5 +8,13 @@ public class ComplexForLoop {
         for(int i = 0; i < 20; i = i + 4){
             System.out.println((b-a) * i);
         }
+
+        for(int i = 1; i < 1024; i = i * 2){
+            System.out.println((b-a) * i);
+        }
+
+        for(int i = 100; i > 20; i = i - 2){
+            System.out.println((b-a) * i);
+        }
     }
 }
