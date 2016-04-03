ca737a8d0acd4a761499a79e735a698c1c803546
Galen Han <Mastergalen@users.noreply.github.com>
2016-04-03 18:41:53 +0200

    Updated NestedIfs test case

--- src/comp207p/target/NestedIfs.java
+++ src/comp207p/target/NestedIfs.java
@@ -3,8 +3,10 @@ package comp207p.target;
 public class NestedIfs {
     public int foo() {
         int a = 10;
+        int b = 4;
 
-        if(a > 4) {
+
+        if(a + b > 4) {
             a+= 20;
 
             if(a > 25) {
