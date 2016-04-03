a05e52b4ded2647020808815775c158407d24e76
Galen Han <Mastergalen@users.noreply.github.com>
2016-04-03 18:18:57 +0200

    Proper while loop folding test

--- src/comp207p/target/WhileLoop.java
+++ src/comp207p/target/WhileLoop.java
@@ -3,11 +3,12 @@ package comp207p.target;
 public class WhileLoop {
     public double foo() {
         double a = 2.2232;
+        double b = 1.7;
 
         int i = 0;
         double you = 0;
         while(i < 10) {
-            you = a * i + 4;
+            you = (a + b) * i + 4;
             System.out.println(you + 4);
             i+= 2;
         }
