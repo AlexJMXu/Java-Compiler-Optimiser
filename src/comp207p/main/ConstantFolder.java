b1934a11e0d5c3ee68a948b3a25cb67171018890
iAlex95 <ajm.xu@outlook.com>
2016-04-05 17:12:51 +0100

    Folding should now check if any variable is dynamic

--- src/comp207p/main/ConstantFolder.java
+++ src/comp207p/main/ConstantFolder.java
@@ -14,31 +14,31 @@ import org.apache.bcel.classfile.*;
 import org.apache.bcel.generic.*;
 import org.apache.bcel.util.InstructionFinder;
 
-import static comp207p.main.utils.ForLoopChecker.checkIfForLoop;
+import static comp207p.main.utils.ForLoopChecker.checkDynamicVariable;
 
 
 public class ConstantFolder
 {
-	ClassParser parser = null;
-	ClassGen gen = null;
+    ClassParser parser = null;
+    ClassGen gen = null;
 
-	JavaClass original = null;
-	JavaClass optimized = null;
+    JavaClass original = null;
+    JavaClass optimized = null;
 
     //Regex for matching an instruction that pushes a value onto the stack
     private static final String LOAD_INSTRUCTION_REGEXP = "(ConstantPushInstruction|LDC|LDC2_W|LoadInstruction)";
 
-	public ConstantFolder(String classFilePath)
-	{
-		try{
-			this.parser = new ClassParser(classFilePath);
-			this.original = this.parser.parse();
-			this.gen = new ClassGen(this.original);
-		} catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-	
+    public ConstantFolder(String classFilePath)
+    {
+        try{
+            this.parser = new ClassParser(classFilePath);
+            this.original = this.parser.parse();
+            this.gen = new ClassGen(this.original);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
     public void write(String optimisedFilePath)
     {
         this.optimize();
@@ -58,11 +58,11 @@ public class ConstantFolder
     /**
      * Initial method
      */
-	public void optimize()
-	{
-		ClassGen cgen = new ClassGen(original);
+    public void optimize()
+    {
+        ClassGen cgen = new ClassGen(original);
         cgen.setMajor(50); //Set major version number of class file to 50 (instead of default 45) to eliminate StackMapFrame errors
-		ConstantPoolGen cpgen = cgen.getConstantPool();
+        ConstantPoolGen cpgen = cgen.getConstantPool();
 
         ConstantPool cp = cpgen.getConstantPool();
         Method[] methods = cgen.getMethods();
@@ -75,8 +75,8 @@ public class ConstantFolder
             optimiseMethod(cgen, cpgen, m); //Optimise each method
         }
         
-		this.optimized = cgen.getJavaClass();
-	}
+        this.optimized = cgen.getJavaClass();
+    }
 
     /**
      * Optimise method instruction list
@@ -150,12 +150,12 @@ public class ConstantFolder
             //Debug output
             System.out.println("==================================");
             System.out.println("Found optimisable negation");
-            Utilities.printInstructionHandles(match, cpgen);
+            Utilities.printInstructionHandles(match, cpgen, instructionList);
 
             InstructionHandle loadInstruction = match[0];
             InstructionHandle negationInstruction = match[1];
 
-            Number value = ValueLoader.getValue(loadInstruction, cpgen);
+            Number value = ValueLoader.getValue(loadInstruction, cpgen, instructionList);
 
             //Multiply by -1 to negate it, inefficient but oh well
             Double negatedValue = Utilities.foldOperation(new DMUL(), value, -1);
@@ -205,7 +205,7 @@ public class ConstantFolder
             //Debug output
             System.out.println("==================================");
             System.out.println("Found optimisable arithmetic set");
-            Utilities.printInstructionHandles(match, cpgen);
+            Utilities.printInstructionHandles(match, cpgen, instructionList);
 
             Number leftValue, rightValue;
             InstructionHandle leftInstruction, rightInstruction, operationInstruction;
@@ -226,13 +226,13 @@ public class ConstantFolder
             }
 
             if (leftInstruction.getInstruction() instanceof LoadInstruction) { //Recognise for loops
-                if (checkIfForLoop(leftInstruction)) {
+                if (checkDynamicVariable(leftInstruction, instructionList)) {
                     Utilities.printDynamicVariableDetected();
                     continue;
                 }
             }
             if (rightInstruction.getInstruction() instanceof LoadInstruction) {
-                if (checkIfForLoop(rightInstruction)) {
+                if (checkDynamicVariable(rightInstruction, instructionList)) {
                     Utilities.printDynamicVariableDetected();
                     continue;
                 }
@@ -240,8 +240,8 @@ public class ConstantFolder
 
             //Fetch values for push instructions
             try {
-                leftValue = ValueLoader.getValue(leftInstruction, cpgen);
-                rightValue = ValueLoader.getValue(rightInstruction, cpgen);
+                leftValue = ValueLoader.getValue(leftInstruction, cpgen, instructionList);
+                rightValue = ValueLoader.getValue(rightInstruction, cpgen, instructionList);
             } catch (UnableToFetchValueException e) {
                 Utilities.printDynamicVariableDetected();
                 continue;
@@ -297,7 +297,7 @@ public class ConstantFolder
             //Debug output
             System.out.println("==================================");
             System.out.println("Found optimisable comparison set");
-            Utilities.printInstructionHandles(match, cpgen);
+            Utilities.printInstructionHandles(match, cpgen, instructionList);
 
             Number leftValue = 0, rightValue = 0;
             InstructionHandle leftInstruction = null, rightInstruction = null, compare = null, comparisonInstruction = null;
@@ -332,13 +332,13 @@ public class ConstantFolder
             }
 
             if (leftInstruction.getInstruction() instanceof LoadInstruction) { //Recognise for loops
-                if (checkIfForLoop(leftInstruction)) {
+                if (checkDynamicVariable(leftInstruction, instructionList)) {
                     Utilities.printDynamicVariableDetected();
                     continue;
                 }
             }
             if (rightInstruction != null && rightInstruction.getInstruction() instanceof LoadInstruction) {
-                if (checkIfForLoop(rightInstruction)) {
+                if (checkDynamicVariable(rightInstruction, instructionList)) {
                     Utilities.printDynamicVariableDetected();
                     continue;
                 }
@@ -353,9 +353,9 @@ public class ConstantFolder
 
             //Fetch values for push instructions
             try {
-                leftValue = ValueLoader.getValue(leftInstruction, cpgen);
+                leftValue = ValueLoader.getValue(leftInstruction, cpgen, instructionList);
                 if (rightInstruction != null) { 
-                    rightValue = ValueLoader.getValue(rightInstruction, cpgen);
+                    rightValue = ValueLoader.getValue(rightInstruction, cpgen, instructionList);
                 }
             } catch (UnableToFetchValueException e) {
                 Utilities.printDynamicVariableDetected();
