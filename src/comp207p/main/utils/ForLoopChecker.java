bae416f7bcacaf359928e0c95a30cb5098397016
iAlex95 <ajm.xu@outlook.com>
2016-04-06 19:26:21 +0100

    Updated DynamicVariable checker

--- src/comp207p/main/utils/ForLoopChecker.java
+++ src/comp207p/main/utils/ForLoopChecker.java
@@ -3,7 +3,46 @@ package comp207p.main.utils;
 import org.apache.bcel.generic.*;
 
 public class ForLoopChecker {
-    /*public static boolean checkDynamicVariable(InstructionHandle h, InstructionList list) {
+    public static boolean checkDynamicVariable(InstructionHandle h, InstructionList list) {
+        if (checkIfCondition(h, list) || checkForLoop(h, list)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean checkIfCondition(InstructionHandle h, InstructionList list) {
+        Instruction checkingInstruction = h.getInstruction();
+        Instruction currentInstruction, currentSubInstruction;
+        InstructionHandle handleIterator = h;
+        while(handleIterator != null) {
+            try {
+                handleIterator = handleIterator.getPrev();
+                currentInstruction = handleIterator.getInstruction();
+                if (currentInstruction instanceof StoreInstruction 
+                    && ((StoreInstruction)currentInstruction).getIndex() == ((LoadInstruction)checkingInstruction).getIndex()) {
+                    InstructionHandle subIterator = handleIterator;
+                    while (subIterator != null) {
+                        subIterator = subIterator.getPrev();
+                        currentSubInstruction = subIterator.getInstruction();
+                        if (currentSubInstruction instanceof BranchInstruction) {
+                            if (((BranchInstruction) currentSubInstruction).getTarget().getPosition() > handleIterator.getPosition()) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        } 
+                    }
+                }
+            } catch (NullPointerException e) {
+                break;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean checkForLoop(InstructionHandle h, InstructionList list) {
         Instruction checkingInstruction = h.getInstruction();
         Instruction currentInstruction, previousInstruction, currentSubInstruction;
         InstructionHandle handleIterator = list.getStart();
@@ -32,39 +71,6 @@ public class ForLoopChecker {
                                 break;
                             }
                         }
-                    }
-                }
-            } catch (NullPointerException e) {
-                break;
-            }
-        }
-
-        return false;
-    }*/
-
-    public static boolean checkDynamicVariable(InstructionHandle h, InstructionList list) {
-        Instruction checkingInstruction = h.getInstruction();
-        Instruction currentInstruction, currentSubInstruction;
-        InstructionHandle handleIterator = h;
-        while(handleIterator != null) {
-            try {
-                handleIterator = handleIterator.getPrev();
-                currentInstruction = handleIterator.getInstruction();
-                if (currentInstruction instanceof StoreInstruction 
-                    && ((StoreInstruction)currentInstruction).getIndex() == ((LoadInstruction)checkingInstruction).getIndex()) {
-                    InstructionHandle subIterator = handleIterator;
-                    while (subIterator != null) {
-                        subIterator = subIterator.getPrev();
-                        currentSubInstruction = subIterator.getInstruction();
-                        if (currentSubInstruction instanceof BranchInstruction) {
-                            if (((BranchInstruction) currentSubInstruction).getTarget().getPosition() > handleIterator.getPosition()) {
-                                return true;
-                            } else {
-                                System.out.println(((BranchInstruction) currentSubInstruction).getTarget().getPosition());
-                                System.out.println(handleIterator.getPosition());
-                                return false;
-                            }
-                        } 
                     }
                 }
             } catch (NullPointerException e) {
