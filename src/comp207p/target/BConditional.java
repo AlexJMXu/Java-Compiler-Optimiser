54e68f78ca6a052d62da29c6880c0a816e0ef596
JAMIE LAW <jamie.law.14@ucl.ac.uk>
2016-04-06 01:14:23 +0100

    Assignment in conditional test to fix

---
+++ src/comp207p/target/BConditional.java
@@ -0,0 +1,18 @@
+package comp207p.target;
+public class BConditional{
+  public void method_one(){
+    int a = 5;
+    boolean b = true;
+    boolean c = false;
+    boolean d = false;
+    boolean e = true;
+
+    if(twelve()){
+      a = 4;
+    }
+    int f = a + 3;
+  }
+  public boolean twelve(){
+    return false;
+  }
+}
